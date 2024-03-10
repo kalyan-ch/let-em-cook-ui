5e365f9c981987f1117ef8c8342315450f7766e0
Kalyan Chavali <kalyan.chavali123@gmail.com>
2024-03-09 17:33:41 -0800

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div>
+      <div className="header">
+      <h1>Let 'em cook</h1>
+      </div>
+      <div className="text">
+        <p>Welcome to let em cook. Now wait for 2000000 days for this project to complete</p>
+      </div>
     </div>
-  );
-}
+    
+  )
+};
 
 export default App;
